a65403a4958518d5851c7fcb08871f570309d435
NickMadero <madero22@students.rowan.edu>
2023-11-21 12:17:26 -0500

    cleaned up snake and setting as most of the code I found not needed at this moment may revisit later on when I need to add to the snake body.

--- Snake/Setting.cs
+++ Snake/Setting.cs
@@ -11,7 +11,7 @@ namespace Snake
         public static int Width { get; set; } // set the width as int class
         public static int Height { get; set; } // set the height as int class
         public static bool GameOver { get; set; } // set the game over as Boolean class
-        public static Direction direction { get; set; } // set the direction as the class we mentioned above
+   
 
 
         public Setting()
@@ -20,7 +20,7 @@ namespace Snake
             Width = 16; // set the width to 16
             Height = 16; // set the height to 16
             GameOver = false; // set game over to false
-            direction = Direction.Down; // the default direction will be down
+           
         }
 
 
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -11,19 +11,10 @@ namespace Snake
     {
         public int X { get; set; }
         public int Y { get; set; }
-        Direction direction; // enum direction 
-        ArrayList snakeBody = new ArrayList();
 
         public Snake() {
             X = 100;
             Y = 100;
-            Point head = new Point(X,Y); // head will always start here
-            snakeBody.Add(head); // add the head to the arraylist
-
-            direction = Direction.Right; // start the snake going to the right
-
-            // for now just starting with a head.
-
         }
 
 
